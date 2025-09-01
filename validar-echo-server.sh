38cf8d61d1ccb38971e70a8efcfad92d2adbf007
Nicolas Deciancio <nicodec89@gmail.com>
2025-08-31 23:38:20 -0300

    ej3 use docker network created by compose

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,7 +2,15 @@
 
 echo "Starting echo server test..."
 TEST_MESSAGE="test_message"
-RESPONSE=$(docker run --rm --network generator_network \
+NETWORK_NAME=$(docker network ls --format "{{.Name}}" | grep -vE "bridge|host|none" | head -1)
+if [[ -n "$NETWORK_NAME" ]]; then
+    echo "Using existing network: $NETWORK_NAME"
+else
+    NETWORK_NAME="generator_network"
+    docker network create $NETWORK_NAME
+    echo "Created new network: $NETWORK_NAME"
+fi
+RESPONSE=$(docker run --rm --network $NETWORK_NAME \
     alpine /bin/sh -c "echo '$TEST_MESSAGE' | nc server 12345")
 
 if [ "$TEST_MESSAGE" == "$RESPONSE" ]; then
